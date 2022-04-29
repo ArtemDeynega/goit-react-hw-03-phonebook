1d298bfa4c24df14143ddf3842e07e0753c3d6f5
artem <73790146+ArtemDeynega@users.noreply.github.com>
2022-04-29 17:00:31 +0300

    done dz

--- src/components/Title/SectionTitle.jsx
+++ src/components/Title/SectionTitle.jsx
@@ -12,5 +12,5 @@ export const SectionTitle = ({ title, children }) => {
 };
 SectionTitle.propTypes = {
   title: PropTypes.string.isRequired,
-  children: PropTypes.element,
+  children: PropTypes.node,
 };
